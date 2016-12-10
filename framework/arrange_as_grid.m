0ea436570891015cc1e8c21e2ce0601a3644a140
Chuan-Zheng Lee <czlee@stanford.edu>
2016-12-10 13:52:31 -0800

    Improve arrange_as_grid script

--- framework/arrange_as_grid.m
+++ framework/arrange_as_grid.m
@@ -1,19 +1,23 @@
+source_dir = '../../../Dropbox/CS 229 Plots/grid-2/plots';
 dest_fig = figure;
-cs = [10 30 100];
+ms = [100 300 1000];
 ns = [10 30 100];
 
-for i = 1:numel(cs)
+% dir_list = arrayfun(@(x) x.name, dir(source_dir), 'UniformOutput', false);
+% dir_list = dir_list(arrayfun(@(x) endsWith(x, '.png') && startsWith(x, 'residues_sgd'), dir_list));
+
+for i = 1:numel(ms)
     for j = 1:numel(ns)
-        c = cs(i);
+        m = ms(i);
         n = ns(j);
-        m = c*n;
+        c = m/n;
         mu = 3e-3/n;
         
         % Code taken from
         % https://www.mathworks.com/matlabcentral/answers/101806-how-can-i-insert-my-matlab-figure-fig-files-into-multiple-subplots
         
         filenameparams = sprintf('m%d_n%d_mu%.0e', m, n, mu);
-        source_fig = openfig(['../../coursework/cs229/project/plots/grid/residues_sgd_' filenameparams], 'reuse');
+        source_fig = openfig(fullfile(source_dir, ['residues_sgd_' filenameparams]));
         source_axes = gca;
         source_objects = source_axes.Children;
         source_title = source_axes.Title.String;
