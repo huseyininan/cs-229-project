7cc9d67f58745d1fb5c52a87c3abf7eeef17cf9c
Chuan-Zheng Lee <czlee@stanford.edu>
2016-12-11 15:18:36 -0800

    Plot multiple m on the same residue plot

--- framework/linear_regression_residue.m
+++ framework/linear_regression_residue.m
@@ -1,19 +1,21 @@
-ms = [100 300 1000];
+ms = [80 120 150 200];
 ns = [10 30 100];
-runs = 200;
+runs = 10;
 iterations = 300;
+M = numel(ms);
 
-for m = ms
-    for n = ns
-        % m = c*n;
+for n = ns
+
+    residue_norms = zeros(runs, M);
+    gd_errors = zeros(runs, M);
+    sgd_errors = zeros(runs, M);
+
+    for j = 1:M
+        m = ms(j);
         c = m/n;
-        mu = 1e-1/n;
+        mu = 1e-3/n;
 
         fprintf('Started c=%d, m=%d, n=%d, mu=%.0e at %s\n', c, m, n, mu, datestr(now))
-
-        residue_norms = zeros(runs, 1);
-        gd_errors = zeros(runs, 1);
-        sgd_errors = zeros(runs, 1);
 
         for i = 1:runs
             % Randomly generate parameters
@@ -24,23 +26,21 @@ for m = ms
             residue_direction = randn(m-n, 1);
             residue_direction = residue_direction / norm(residue_direction);
             residue = null(A')*residue_direction*sqrt(rand())*1000;
-            residue_norms(i) = norm(residue);
+            residue_norms(i,j) = norm(residue);
 
             y = A*theta_true + residue;
             [~, gd_error, ~, ~, sgd_error, ~, ~] = run_linear_regression(A, y, mu, iterations);
-            gd_errors(i) = gd_error(end); % take just the last error
-            sgd_errors(i) = sgd_error(end); % take just the last error
+            gd_errors(i,j) = gd_error(end); % take just the last error
+            sgd_errors(i,j) = sgd_error(end); % take just the last error
 
             % Admin stuff
             fprintf('Completed %d out of %d runs\r', i, runs)
-            save('data_backup.mat', 'm', 'n', 'mu', 'iterations', 'residue_norms', 'gd_errors', 'sgd_errors')
+            save('data_backup.mat', 'i', 'm', 'ms', 'n', 'mu', 'iterations', 'residue_norms', 'gd_errors', 'sgd_errors')
         end
-
-        %% Generate a plot
-        plot_residue
-
     end
+    
+    %% Generate a plot
+    plot_residue
 end
 
 fprintf('Finished at %s\n', datestr(now))
-
--- framework/plot_residue.m
+++ framework/plot_residue.m
@@ -1,24 +1,29 @@
 % Plots residue vs error scatter plots and saves to disk.
 % Requires the following variables to exist prior to script execution:
-%  'm', 'n', 'mu', 'iterations', 'residue_norms', 'gd_errors', 'sgd_errors'
+%  'ms', 'n', 'mu', 'iterations', 'residue_norms', 'gd_errors', 'sgd_errors'
 
-filenameparams = sprintf('m%d_n%d_mu%.0e', m, n, mu);
+filenameparams = sprintf('n%d_mu%.0e', n, mu);
 
 if ~exist('data', 'dir')
     mkdir('data')
 end
-save(['data/data_' filenameparams '.mat'], 'm', 'n', 'mu', 'iterations', 'residue_norms', 'gd_errors', 'sgd_errors')
+save(['data/data_' filenameparams '.mat'], 'ms', 'n', 'mu', 'iterations', 'residue_norms', 'gd_errors', 'sgd_errors')
 
 if ~exist('plots', 'dir')
     mkdir('plots')
 end
 
+legend_by_ms = arrayfun(@(x) sprintf('m = %d', x), ms, 'UniformOutput', false);
+markers = {'o'; '+'; 's'; '^'; 'x'};
+
 fig1 = figure(1);
 clf
-plot(residue_norms.^2, gd_errors.^2, '+')
-title(sprintf('Gradient descent (m = %d, n = %d, \\mu = %.1e)', m, n, mu))
+gd_plots = plot(residue_norms.^2, gd_errors.^2);
+title(sprintf('Gradient descent (n = %d, \\mu = %.1e)', n, mu))
 xlabel('||residue||^2')
 ylabel(sprintf('|| \\theta^{(%d)} - \\theta_{opt} ||^2', iterations))
+legend(legend_by_ms)
+set(gd_plots, {'Marker'}, markers(1:numel(ms)), 'LineStyle', 'none')
 filename = ['plots/residues_gd_' filenameparams];
 savefig(fig1, filename, 'compact')
 saveas(fig1, [filename '.eps'])
@@ -26,10 +31,12 @@ saveas(fig1, [filename '.png'])
 
 fig2 = figure(2);
 clf
-plot(residue_norms.^2, sgd_errors.^2, '+')
-title(sprintf('Stochastic gradient descent (m = %d, n = %d, \\mu = %.1e)', m, n, mu))
+sgd_plots = plot(residue_norms.^2, sgd_errors.^2);
+title(sprintf('Stochastic gradient descent (n = %d, \\mu = %.1e)', n, mu))
 xlabel('||residue||^2')
 ylabel(sprintf('|| \\theta^{(%d)} - \\theta_{opt} ||^2', iterations))
+legend(legend_by_ms)
+set(sgd_plots, {'Marker'}, markers(1:numel(ms)), 'LineStyle', 'none')
 filename = ['plots/residues_sgd_' filenameparams];
 savefig(fig2, filename, 'compact')
 saveas(fig2, [filename '.eps'])
